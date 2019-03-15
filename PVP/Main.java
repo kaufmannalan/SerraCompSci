6a3ef29ef3f8935310e15fdff35e7896ce002201
kaufmannalan <42677923+kaufmannalan@users.noreply.github.com>
2019-03-15 09:37:08 -0500

    Update Main.java

--- PVP/Main.java
+++ PVP/Main.java
@@ -2,17 +2,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Fighter playerA = new Fighter("a");
+        Fighter playerA = new Fighter("Fredericka McWhirr");
         playerA.showStats();
 
-        Mage playerB = new Mage("b");
+        Mage playerB = new Mage("Skeletor");
         playerB.showStats();
 
         Paladin playerC = new Paladin("Thonk Thunderhamerr");
         playerC.showStats();
 
-        Priest playerD = new Priest("Sh'ar M'kal-masal-safari");
+        Priest playerD = new Priest("Sh'ar M'kal-masal-safari, The Radiant, Md.");
         playerD.showStats();
+        
+        Priest playerE = new Druid("Zephyr Whitefeather");
+        playerE.showStats();
 
 
         System.out.println(PlayerCharacter.numCharacters());
