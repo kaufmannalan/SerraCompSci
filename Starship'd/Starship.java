5767ea591a417ac7460a712ee2035c45917c0f6a
kaufmannalan <42677923+kaufmannalan@users.noreply.github.com>
2019-04-01 09:40:12 -0500

    Create Starship.java

---
+++ Starship'd/Starship.java
@@ -0,0 +1,12 @@
+public abstract class Starship {
+    protected int hull;
+    protected int shield;
+    protected int beam;
+    protected int torp;
+    protected String name;
+
+    public void showStats(){
+
+    }
+
+}
